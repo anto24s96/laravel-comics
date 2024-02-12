7a244b1ea732fe8755a93df335b55d8558bf8433
anto24s96 <anto24s96@gmail.com>
2024-02-12 15:36:16 +0100

    [UPDATE] added header section

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <header class="container d-flex">
+        <a href="#">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo_header">
+        </a>
+        <nav>
+            <ul class="d-flex">
+                <li><a href="#">characters</a></li>
+                <li><a href="#">comics</a></li>
+                <li><a href="#">movies</a></li>
+                <li><a href="#">tv</a></li>
+                <li><a href="#">games</a></li>
+                <li><a href="#">collectibles</a></li>
+                <li><a href="#">videos</a></li>
+                <li><a href="#">fans</a></li>
+                <li><a href="#">news</a></li>
+                <li><a href="#">shop</a></li>
+            </ul>
+        </nav>
+    </header>
+</header>
